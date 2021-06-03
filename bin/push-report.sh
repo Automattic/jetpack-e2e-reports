78c7828acfc70b3f276946582700cab95fd8cc7f
Adrian Moldovan <adim.moldovan@gmail.com>
2021-06-03 19:13:21 +0300

    Push report changes

--- bin/push-report.sh
+++ bin/push-report.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
 set -eo pipefail
+
+USERNAME="github-actions-bot"
+EMAIL="$USERNAME@users.noreply.github.com"
+
+# commit and push
+if [ -z "$(git status --porcelain)" ]; then
+  echo "There are no changes to deploy"
+else
+  git config --local user.name "$USERNAME"
+  git config --local user.email "$EMAIL"
+  git add .
+  git commit -m "Publish new test reports"
+  git push
+fi
