2acd7f2a9f1a340bcb39be323ced4aef851d4660
Adrian Moldovan <adim.moldovan@gmail.com>
2021-06-03 16:53:34 +0300

    Check reports prerequisites

--- bin/generate-report.sh
+++ bin/generate-report.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
+# Required:
+# - RESULTS_PATH: Path to the test output directory
+# - PR_NUMBER: The number of the Jetpack PR that triggered this workflow
+# - BRANCH: The name of the Jetpack branch (excluded by PR_NUMBER)
+
 set -eo pipefail
 
+# Test Allure installation
 allure --version
+
+if [[ -z "$RESULTS_PATH" ]]; then
+	echo "::error::RESULTS_PATH must be set"
+	exit 1
+elif [[ ! -d "$RESULTS_PATH" ]]; then
+	echo "::error::'$RESULTS_PATH' does not exist or is not a directory"
+	exit 1
+fi
+
+if [[ -z "$PR_NUMBER" ]]; then
+	echo "PR_NUMBER is not defined, using BRANCH"
+
+	if [[ -z "$BRANCH" ]]; then
+	echo "::error::PR_NUMBER or BRANCH is not defined"
+  exit 1
+fi
+
+
+ls -la
--- bin/install-allure.sh
+++ bin/install-allure.sh
@@ -2,17 +2,20 @@
 
 set -eo pipefail
 
+# Test Java installation
 java -version
 
-echo "Installing Allure"
 ALLURE_VERSION=2.14.0
 ALLURE_DOWNLOAD_URL=https://github.com/allure-framework/allure2/releases/download/$ALLURE_VERSION/allure-$ALLURE_VERSION.zip
+
+echo "Installing Allure $ALLURE_VERSION"
 wget --no-verbose -O allure.zip $ALLURE_DOWNLOAD_URL \
   && unzip allure.zip -d . \
   && rm -rf allure.zip \
 
+# Add Allure in Github PATH to make it available to all subsequent actions in the current job
 ALLURE_PATH=$PWD/allure-$ALLURE_VERSION/bin
-export PATH="$ALLURE_PATH:$PATH"
-allure --version
-
 echo "$ALLURE_PATH" >> "$GITHUB_PATH"
+
+# Test Allure installation
+allure --version
