df4e11ecc65695e2c6f23c2ead812193ffd1a53a
Adrian Moldovan <adim.moldovan@gmail.com>
2021-06-03 16:00:53 +0300

    Install Allure

--- bin/install-allure.sh
+++ bin/install-allure.sh
@@ -2,12 +2,12 @@
 
 set -eo pipefail
 
-javac -version
+java -version
 
 echo "Installing Allure"
 ALLURE_VERSION=2.14.0
 ALLURE_DOWNLOAD_URL=https://github.com/allure-framework/allure2/releases/download/$ALLURE_VERSION/allure-$ALLURE_VERSION.zip
-wget -O /tmp/allure.zip $ALLURE_DOWNLOAD_URL \
-  && unzip /tmp/allure.zip -d /
+wget --no-verbose -O /tmp/allure.zip $ALLURE_DOWNLOAD_URL \
+  && unzip /tmp/allure.zip -d .
 
 allure --version
