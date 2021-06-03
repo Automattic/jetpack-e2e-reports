c3f51f8c524a742577f72f00904c263b66a58909
Adrian Moldovan <adim.moldovan@gmail.com>
2021-06-03 16:56:23 +0300

    Check reports prerequisites

--- bin/install-allure.sh
+++ bin/install-allure.sh
@@ -13,9 +13,11 @@ wget --no-verbose -O allure.zip $ALLURE_DOWNLOAD_URL \
   && unzip allure.zip -d . \
   && rm -rf allure.zip \
 
-# Add Allure in Github PATH to make it available to all subsequent actions in the current job
 ALLURE_PATH=$PWD/allure-$ALLURE_VERSION/bin
-echo "$ALLURE_PATH" >> "$GITHUB_PATH"
 
 # Test Allure installation
+export PATH="$ALLURE_PATH:$PATH"
 allure --version
+
+# Add Allure in Github PATH to make it available to all subsequent actions in the current job
+echo "$ALLURE_PATH" >> "$GITHUB_PATH"
