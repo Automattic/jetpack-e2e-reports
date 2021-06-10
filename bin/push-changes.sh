b5c6ea4676657eb8e603667868fc880694cff3b6
Adrian Moldovan <adim.moldovan@gmail.com>
2021-06-10 18:52:06 +0300

    Split summary and dashboard into a different job

--- bin/push-changes.sh
+++ bin/push-changes.sh
@@ -16,6 +16,7 @@ if [ -z "$(git status --porcelain)" ]; then
 else
   git config --local user.name "$USERNAME"
   git config --local user.email "$EMAIL"
+  git pull
   git add .
   git commit -m "$MESSAGE"
   git push
