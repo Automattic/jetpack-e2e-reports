e687bae757b44c645e0424ddee97c40fdc82e7fa
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2021-12-20 12:54:21 +0800

    update performance page

--- src/components/Performance.js
+++ src/components/Performance.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactGA from 'react-ga';
+import { Container, Row, Col } from 'react-bootstrap';
 
 import ReactEcharts from 'echarts-for-react';
 
@@ -7,58 +8,56 @@ export default class Performance extends React.Component {
 	constructor( props ) {
 		super( props );
 		this.state = {
+			selected: 'type',
 			rawData: [],
 			isDataFetched: false,
+			metrics: [ 'serverResponse', 'firstPaint', 'domContentLoaded', 'loaded', 'firstContentfulPaint', 'firstBlock', 'type', 'focus', 'listViewOpen', 'inserterOpen', 'inserterHover', 'inserterSearch' ],
 		};
+	}
+
+	calcPercent( base, comp ) {
+		return Math.round( ( ( ( comp / base ) - 1 ) * 100 ) * 100 ) / 100;
+	}
+
+	prettyTitle( title ) {
+		return title.replace( /([a-z0-9])([A-Z])/g, '$1 $2' ).toUpperCase();
 	}
 
 	prepareChartData( jsonData ) {
 		const result = {};
+		const metrics = Object.keys( jsonData[ jsonData.length - 1 ].baseAvg );
 
-		const calcPercent = ( base, comp ) => ( comp / base - 1 ) * 100;
-
-		const byDate = {};
-		jsonData.forEach( ( record ) => {
-			let date = new Date( record.date );
-			date = date.toISOString().split( 'T' )[ 0 ];
-			if ( ! byDate[ date ] ) {
-				byDate[ date ] = [];
-			}
-
-			const percentage = Object.keys( record.baseAvg ).reduce(
-				( acc, metric ) => {
-					acc[ metric ] = calcPercent(
-						record.baseAvg[ metric ],
-						record.jetpackAvg[ metric ]
-					);
-					return acc;
-				},
-				{}
-			);
-
-			byDate[ date ].push( percentage );
-		} );
-
-		Object.entries( byDate ).forEach( ( [ date, recArry ] ) => {
-			const size = recArry.length;
-			result[ date ] = { date };
-
-			// looping through all the keys, and calculating average for specific day
-			Object.keys( recArry[ 0 ] ).forEach( ( metric ) => {
-				// sum
-				result[ date ][ metric ] = recArry.reduce(
-					( a, rec ) => a + rec[ metric ],
-					0
-				);
-				// calculating average
-				result[ date ][ metric ] /= size;
-				// rounding
-				result[ date ][ metric ] =
-					Math.round( result[ date ][ metric ] * 100 ) / 100;
+		jsonData.forEach( obj => {
+			metrics.forEach( metric => {
+				if ( ! result[ metric ] ) {
+					result[ metric ] = [];
+				}
+				let date = new Date( obj.date );
+				date = date.toISOString().split( 'T' )[ 0 ];
+				result[ metric ].push( { base: obj.baseAvg[ metric ], jetpack: obj.jetpackAvg[ metric ], date } );
 			} );
 		} );
 
-		return Object.values( result );
+		const out = {};
+
+		metrics.forEach( metric => {
+			const dates = [ ...new Set( result[ metric ].map( entry => entry.date ) ) ];
+			const entries = dates.reduce( ( acc, date ) => {
+				const byDate = result[ metric ].filter( e => e.date === date );
+
+				acc.push( {
+					base: Math.round( byDate.reduce( ( acc, e ) => acc += e.base, 0 ) / byDate.length ),
+					jetpack: Math.round( byDate.reduce( ( acc, e ) => acc += e.jetpack, 0 ) / byDate.length ),
+					date,
+				} );
+
+				return acc;
+			}, [] );
+
+			out[ metric ] = entries;
+		} );
+
+		return out;
 	}
 
 	async componentDidMount() {
@@ -80,6 +79,140 @@ export default class Performance extends React.Component {
 		ReactGA.pageview( 'performance' );
 	}
 
+	renderChart( type, chartData ) {
+		const chartOptions = {
+			grid: {
+				left: 60,
+				right: 0,
+			},
+			title: {
+				text: this.prettyTitle( type ),
+				textStyle: {
+					color: '#cccccc',
+					fontSize: '0.9rem',
+				},
+			},
+			tooltip: {
+				trigger: 'axis',
+				axisPointer: {
+					type: 'cross',
+				},
+			},
+			xAxis: [
+				{
+					type: 'category',
+					data: chartData.map( function( e ) {
+						return e.date;
+					} ),
+				},
+			],
+			yAxis: [
+				{
+					type: 'value',
+					splitLine: {
+						lineStyle: {
+							type: 'dotted',
+							color: '#6b6d76',
+						},
+					},
+				},
+				{
+					type: 'value',
+					splitLine: {
+						lineStyle: {
+							type: 'dashed',
+							color: 'rgba(107,109,118,0.47)',
+						},
+					},
+					min: 0,
+					axisLabel: {
+						formatter: '{value} %',
+					},
+				},
+			],
+			dataZoom: [
+				{
+					type: 'inside',
+				},
+				{
+					start: 50,
+					end: 100,
+				},
+			],
+			series: [
+				{
+					name: 'base',
+					type: 'line',
+					emphasis: {
+						focus: 'series',
+					},
+					color: 'rgb(99,100,138)',
+					data: chartData.map( function( e ) {
+						return e.base;
+					} ),
+				},
+				{
+					name: 'jetpack',
+					type: 'line',
+					emphasis: {
+						focus: 'series',
+					},
+					color: 'rgb(99,150,138)',
+					data: chartData.map( function( e ) {
+						return e.jetpack;
+					} ),
+				},
+			],
+		};
+
+		return <ReactEcharts option={ chartOptions }
+			style={ { height: '400px', width: '100%' } }
+		/>;
+	}
+
+	onSelect( type ) {
+		this.setState( { selected: type } );
+	}
+
+	renderTypeInfo( type, last, prev ) {
+		const diffPercent = this.calcPercent( prev, last );
+
+		return (
+			<Row style={ { justifyContent: 'space-between' } }>
+				<div style={ { display: 'flex' } }><h5>{ type }: { last } ms.</h5>&nbsp;from: { prev }ms.</div>
+				<span>&nbsp;VS previous: { diffPercent }ms.</span>
+			</Row>
+		);
+	}
+
+	renderContainer( type, data ) {
+		const last = data.at( -1 );
+		const prev = data.at( -2 );
+
+		return <Container className="perf-button" onClick={ () => this.onSelect( type ) }>
+			<Row><h4>{ this.state.selected === type ? <u>{ this.prettyTitle( type ) }</u> : this.prettyTitle( type ) }</h4></Row>
+			<Row>&nbsp;</Row>
+			{ this.renderTypeInfo( 'Base', last.base, prev.base ) }
+			{ this.renderTypeInfo( 'Jetpack', last.jetpack, prev.jetpack ) }
+		</Container>;
+	}
+
+	renderButtons( chartData ) {
+		return <Container fluid>
+			<Row>
+				<Col sm>{ this.renderContainer( 'type', chartData.type ) }</Col>
+				<Col sm>{ this.renderContainer( 'loaded', chartData.loaded ) }</Col>
+				<Col sm>{ this.renderContainer( 'focus', chartData.focus ) }</Col>
+			</Row>
+			<Row>
+				<Col sm>{ this.renderContainer( 'inserterOpen', chartData.inserterOpen ) }</Col>
+				<Col sm>{ this.renderContainer( 'inserterHover', chartData.inserterHover ) }</Col>
+				<Col sm>{ this.renderContainer( 'inserterSearch', chartData.inserterSearch ) }</Col>
+
+			</Row>
+		</Container>;
+	}
+
 	render() {
 		if ( ! this.state.isDataFetched ) {
 			return null;
@@ -87,101 +220,12 @@ export default class Performance extends React.Component {
 
 		const chartData = this.prepareChartData( this.state.rawData );
 
-		const chartOptions = [];
-
-		Object.keys( chartData[ 0 ] ).forEach( key => {
-			if ( key !== 'date' ) {
-				chartOptions.push( {
-					grid: {
-						left: 60,
-						right: 0,
-					},
-					title: {
-						text: key.replace( /([a-z0-9])([A-Z])/g, '$1 $2' ).toUpperCase(),
-						textStyle: {
-							color: '#cccccc',
-							fontSize: '0.9rem',
-						},
-					},
-					tooltip: {
-						trigger: 'axis',
-						axisPointer: {
-							type: 'cross',
-						},
-					},
-					// legend: {
-					// 	textStyle: {
-					// 		color: '#f5f5f5',
-					// 	},
-					// },
-					xAxis: [
-						{
-							type: 'category',
-							data: chartData.map( function( e ) {
-								return e.date;
-							} ),
-						},
-					],
-					yAxis: [
-						{
-							type: 'value',
-							splitLine: {
-								lineStyle: {
-									type: 'dotted',
-									color: '#6b6d76',
-								},
-							},
-						},
-						{
-							type: 'value',
-							splitLine: {
-								lineStyle: {
-									type: 'dashed',
-									color: 'rgba(107,109,118,0.47)',
-								},
-							},
-							min: 0,
-							axisLabel: {
-								formatter: '{value} %',
-							},
-						},
-					],
-					dataZoom: [
-						{
-							type: 'inside',
-						},
-						{
-							start: 50,
-							end: 100,
-						},
-					],
-					series: [
-						{
-							name: key,
-							type: 'bar',
-							emphasis: {
-								focus: 'series',
-							},
-							color: 'rgb(99,150,138)',
-							data: chartData.map( function( e ) {
-								return e[ key ];
-							} ),
-						},
-					],
-				} );
-			}
-		} );
-
+		const type = this.state.selected;
 		return (
 			<div>
 				<h4>Performance metrics</h4>
-				{ chartOptions.map( ( option, key ) =>
-					<div key={ key }>
-						<ReactEcharts option={ option } />
-						<hr />
-					</div>
-				) }
-
+				{ this.renderButtons( chartData ) }
+				{ this.renderChart( type, chartData[ type ] ) }
 			</div>
 		);
 	}
