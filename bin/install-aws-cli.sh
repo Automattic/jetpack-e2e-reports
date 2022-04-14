274a4ad9c381789b1025bf8a1dbb02baa477032c
Adrian Moldovan <adim.moldovan@gmail.com>
2022-04-14 20:58:57 +0300

    * Install aws-cli

--- bin/install-aws-cli.sh
+++ bin/install-aws-cli.sh
@@ -3,9 +3,9 @@
 set -eo pipefail
 
 # Install aws-cli
-echo "Installing aws-cli"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
+#echo "Installing aws-cli"
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip
+#./aws/install
 # Test aws-cli installation
 aws --version
