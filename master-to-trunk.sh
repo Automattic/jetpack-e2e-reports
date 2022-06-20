1074882afb857ade0b62018035966819a56dd07e
Adrian Moldovan <adim.moldovan@gmail.com>
2022-06-20 14:41:54 +0300

    * Fix master-to-trunk.sh

--- master-to-trunk.sh
+++ master-to-trunk.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-aws s3 sync s3://a8c-jetpack-e2e-reports/data /public/data --delete
+aws s3 sync s3://a8c-jetpack-e2e-reports/data ./public/data --delete
 
 bk_dir="./public/data-backup-$(date +"%Y%m%dT%H%M%S")"
 cp -R ./public/data "$bk_dir"
@@ -11,4 +11,4 @@ sed -i 's/\"master\"\:/\"trunk\"\:/g' ./public/data/results-daily.json
 sed -i 's/\"master\"\:/\"trunk\"\:/g' ./public/data/results-weekly.json
 sed -i 's/\"master\"\:/\"trunk\"\:/g' ./public/data/results-monthly.json
 
-#aws s3 sync /public/data s3://a8c-jetpack-e2e-reports/data /public/data
+aws s3 sync ./public/data s3://a8c-jetpack-e2e-reports/data
