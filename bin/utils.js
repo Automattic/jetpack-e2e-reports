f36eabbadb60dea4ef63ddbac636ccab148e39df
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-08-18 11:24:50 +0300

    Fix cleanTrace function to replace Cloudflare URLs with a placeholder

--- bin/utils.js
+++ bin/utils.js
@@ -38,6 +38,7 @@ function cleanTrace( trace ) {
 		.replace( /\n+/g, '\n' )
 		.replace( /at .+/gs, trace.match( /at .+/ ) ) // keep only the first "at" line
 		.replace( /https:\/\/.+.a8c-localtunnel.cyou/g, 'SITE-URL' )
+		.replace( /https:\/\/.+.trycloudflare.com/g, 'SITE-URL' )
 		.replace(
 			/waiting for selector "\.wp-block-jetpack-.+ \.components-sandbox" to be visible/g,
 			'waiting for selector ".wp-block-jetpack-BLOCK .components-sandbox" to be visible'
