eeea7a79bc45fcb91fdf84423f6a660c7b531d64
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-08-19 02:05:30 +0300

    Sanitize call logs in cleanTrace function

--- bin/utils.js
+++ bin/utils.js
@@ -39,6 +39,8 @@ function cleanTrace( trace ) {
 		.replace( /at .+/gs, trace.match( /at .+/ ) ) // keep only the first "at" line
 		.replace( /(https?:\/\/)?[^\s]+\.a8c-localtunnel\.cyou/g, 'SITE-URL' )
 		.replace( /(https?:\/\/)?[^\s]+\.trycloudflare\.com/g, 'SITE-URL' )
+		.replace( /Call log:(?:\n\s*-.+){3,}/g, 'Call log: [...]' )
+		.replace( /cookie: .*/gi, 'cookie: [...]' )
 		.replace(
 			/waiting for selector "\.wp-block-jetpack-.+ \.components-sandbox" to be visible/g,
 			'waiting for selector ".wp-block-jetpack-BLOCK .components-sandbox" to be visible'
