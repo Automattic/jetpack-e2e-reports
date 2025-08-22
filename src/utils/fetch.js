5219068ba01caee5e0431b218e1b03fd5bf83821
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-08-22 17:48:15 +0300

    fix: improve error handling in fetchJsonData function

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,7 +1,9 @@
 export async function fetchJsonData( url ) {
 	const response = await fetch( url );
 	if ( ! response.ok ) {
-		console.error( `Error fetching data from ${ url } ${ response.status }` );
+		const errorMessage = `Error fetching data from ${ url }: ${ response.status } ${ response.statusText }`;
+		console.error( errorMessage );
+		throw new Error( errorMessage );
 	}
 	return await response.json();
 }
